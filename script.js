01bfeefeddc55021d16a26e4a9d2426a8c90660b
Timmy Antonio <tgantonio.jr@gmail.com>
2021-03-12 11:53:41 +0000

    add checking winner logic

--- script.js
+++ script.js
@@ -2,37 +2,27 @@ let dolphinsAverageScore;
 let koalasAverageScore;
 
 const dolphinsScores = {
-    firstAttempt : 20,
-    secondAttempt : 30,
-    thirdAttempt : 40,
-}
+  firstAttempt: 345,
+  secondAttempt: 345,
+  thirdAttempt: 40,
+};
 
 const koalasScores = {
-    firstAttempt : 20,
-    secondAttempt : 30,
-    thirdAttempt : 40,
-}
+  firstAttempt: 234,
+  secondAttempt: 234,
+  thirdAttempt: 3,
+};
 
-const calculateTeamAverage =(scores) => {
-    return (scores.firstAttempt + scores.secondAttempt + scores.thirdAttempt) / 3;
-}
+const calculateTeamAverage = scores =>
+  (scores.firstAttempt + scores.secondAttempt + scores.thirdAttempt) / 3;
 
-const result = (dolphinsScores,koalasScores) => {
-    dolphinsAverageScore = calculateTeamAverage(dolphinsScores);
-    koalasAverageScore = calculateTeamAverage(koalasScores);
-    const higherScoreTeam = dolphinsAverageScore > koalasAverageScore ? "Dolphin" : dolphinsAverageScore === koalasAverageScore ? "TIE" : "Koalas";
-    
-    switch (higherScoreTeam) {
-        case "Dolphin":
-            if(dolphinsAverageScore >= (koalasAverageScore * 2 )) return `${higherScoreTeam} win (${dolphinsAverageScore} vs. ${koalasAverageScore})`; 
-        case "Koalas":
-            if(koalasAverageScore >= (dolphinsAverageScore * 2 )) return `${higherScoreTeam} win (${koalasAverageScore} vs. ${dolphinsAverageScore})`;       
-    
-        default:
-            return "No team wins!"
-    }
-}
-
-
-const res = result(dolphinsScores,koalasScores);
-console.log(res)
+const checkWinner = (dolphinsScores, koalasScores) => {
+  dolphinsAverageScore = calculateTeamAverage(dolphinsScores);
+  koalasAverageScore = calculateTeamAverage(koalasScores);
+  if (dolphinsAverageScore >= koalasAverageScore * 2)
+    return `Dolphins wins (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+  else if (koalasAverageScore >= dolphinsAverageScore * 2)
+    return `Koalas wins (${koalasAverageScore} vs. ${dolphinsAverageScore})`;
+  else return 'No one wins...';
+};
+console.log(checkWinner(dolphinsScores, koalasScores));
